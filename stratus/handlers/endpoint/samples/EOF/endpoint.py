8e0e77ab0496711504ea80b168fc2fd06eda7277
Lakshmi Priyanka <lakshmipriyanka@Lakshmis-MacBook-Pro.local>
2019-10-17 16:43:01 -0400

    Created new package for EOF

--- stratus/handlers/endpoint/samples/EOF/endpoint.py
+++ stratus/handlers/endpoint/samples/EOF/endpoint.py
@@ -4,6 +4,8 @@ from stratus_endpoint.util.config import StratusLogger
 from stratus_endpoint.handler.execution import Executable, ExecEndpoint
 import xarray as xa
 import abc
+import numpy as np
+from eofs.xarray import Eof
 
 
 class XaOpsEndpoint(ExecEndpoint):
@@ -71,30 +73,29 @@ class XaOpsExecutable(Executable):
             Returns:
             TaskResult: The result of the operation.
             """
-        print( f"Executing request {self.request}" )
+        print(f"Executing request {self.request}")
         inputSpec = self.request.get('input', [])
-        dset: xa.Dataset = xa.open_dataset(inputSpec['uri'])
+        dset: xa.Dataset = xa.open_dataset(inputSpec['filename'])
         vid = inputSpec['name']
-        variable: xa.DataArray = dset.data_vars[vid]
+        variable: xa.DataArray = dset[vid]
         result_arrays = self.operate(vid, variable)
-        resultDataset = xa.Dataset(result_arrays, dset.coords, dset.attrs)
-        return TaskResult(kwargs, [resultDataset])
+        # resultDataset = xa.Dataset( result_arrays, dset.coords, dset.attrs)
+        return TaskResult(kwargs, [result_arrays])
 
-    def operate(self, vid: str, variable: xa.DataArray )-> Dict[str,xa.DataArray] :
+    def operate(self, vid: str, variable: xa.DataArray) -> Dict[str, xa.DataArray]:
         """
             Convenience method defined for this particular operation
         """
         opSpecs = self.request['operation']
-        result_arrays: Dict[str,xa.DataArray] = {}
+        result_arrays: Dict[str, xa.DataArray] = {}
         for opSpec in opSpecs:
             opId = opSpec['name'].split(':')[1]
             opAxis = opSpec['axis']
-            new_vid = "-".join([vid, opAxis, opId])
-            if   opId == "mean": result_arrays[new_vid] = variable.mean( dim=opAxis )
-            elif opId == "ave":  result_arrays[new_vid] = variable.mean( dim=opAxis)
-            elif opId == "max":  result_arrays[new_vid] = variable.max( dim=opAxis )
-            elif opId == "min":  result_arrays[new_vid] = variable.min( dim=opAxis )
-            elif opId == "sum":  result_arrays[new_vid] = variable.sum( dim=opAxis )
-            elif opId == "std":  result_arrays[new_vid] = variable.std( dim=opAxis )
-            else: raise Exception( f"Unknown operation: '{opId}'")
+            coslat = np.cos(np.deg2rad(variable.coords['latitude'].values))
+            wgts = np.sqrt(coslat)[..., np.newaxis]
+            solver = Eof(variable, weights=wgts)
+            if opId == "correlation":
+                result_arrays = solver.eofsAsCorrelation(neofs=1)
+            else:
+                result_arrays = solver.eofsAsCovariance(neofs=1)
         return result_arrays
